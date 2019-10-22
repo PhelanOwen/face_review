8af63407f4778392e46d8a2e196c0d39f0085a64
PhelanOwen <phelanowen@gmail.com>
2019-10-22 19:36:21 +0100

    Update categorise.py
    Should sort faces into folders, where subfolder name is the face rating

--- categorise.py
+++ categorise.py
@@ -5,14 +5,25 @@ files = [f for f in shutil.os.listdir('.') if '.jpg' in f]
 try:
     shutil.os.mkdir('training')
     shutil.os.mkdir('validation')
+    shutil.os.mkdir('score_info')
+
+except:
+    pass
+
+try:
+    for i in range(0,6):
+        shutil.os.mkdir('training/' + str(i))
+        shutil.os.mkdir('validation/' + str(i))
 except:
     pass
 
 for i,f in enumerate(files):
     res = 'r' + f[1:-3] + 'txt'
+    score = int(open(res, 'r').read())
+
     if i%2 == 0:
-        shutil.move(f, 'training/'+f)
-        shutil.move(res, 'training/'+res)
+        shutil.move(f, 'training/'+str(score) +'/' + f)
     else:
-        shutil.move(f, 'validation/'+f)
-        shutil.move(res, 'validation/'+res)
+        shutil.move(f, 'validation/'+str(score) +'/' + f)
+
+    shutil.move(res, 'score_info/'+res)
